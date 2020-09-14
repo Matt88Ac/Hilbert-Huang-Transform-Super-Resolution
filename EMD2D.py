83c74060d22bcbd0f48c430cc0c80bc3fa52a0bd
Matt8ac <Matt8ac@gmail.com>
2020-09-14 18:23:39 +0300

    ..

--- EMD2D.py
+++ EMD2D.py
@@ -29,65 +29,42 @@ class EMD2D:
             return
 
         self.EMD = EMD
-        self.img = None
-        if save:
-            self.img = image
+        self.img = image
 
         self.shape = image.shape
-
-        def emd_images_col(colOfImage: np.ndarray):
-            return self.EMD(colOfImage).decompose()
-
-        def concatZeros(row: np.ndarray, howMuch: int, bo) -> np.ndarray:
-            if len(row.shape) == 1:
-                return np.vstack((row, np.zeros((howMuch, row.shape[0]))))
-            return np.vstack((row, np.zeros((howMuch, row.shape[1]))))
-
-        def checkZeroPad(imfs: np.ndarray):
-            return self.NoIMFs - imfs.shape[0]
-
-        def AddDecomposed(newIMF: np.ndarray):
-            newArr = np.array([np.vstack((self.IMFs[0], newIMF[0]))])
-            n = checkZeroPad(newIMF)
-            for i in range(1, min(self.NoIMFs, newIMF.shape[0])):
-                newArr = np.vstack((newArr, np.array([np.vstack((self.IMFs[i], newIMF[i]))])))
-            if n == 0:
-                self.IMFs = newArr.copy()
-                return
-            elif n < 0:
-                for i in range(self.NoIMFs, newIMF.shape[0]):
-                    toAdd = newArr[0].shape[0] - 1
-                    ta = np.array([concatZeros(newIMF[i], toAdd, False)])
-                    newArr = np.vstack((newArr, ta))
-                self.IMFs = newArr.copy()
-                self.NoIMFs = newIMF.shape[0]
-                return
-
-            for i in range(newIMF.shape[0], self.NoIMFs):
-                ta = np.array([concatZeros(self.IMFs[i], 1, True)])
-                newArr = np.vstack((newArr, ta))
-            self.IMFs = newArr.copy()
-
-        def Run(img: np.ndarray) -> np.ndarray:
-            deco = emd_images_col(img[:, 0])
-            self.NoIMFs = deco.shape[0]
-            self.IMFs = deco.copy()
-
-            for i in range(1, img.shape[1]):
-                deco = emd_images_col(img[:, i])
-                AddDecomposed(deco.copy())
-            return self.IMFs.copy()
-
-        if len(image.shape) == 3:
-            No = 0
-            self.Rs = Run(image[:, :, 0])
-            No += self.NoIMFs
-            self.Gs = Run(image[:, :, 1])
-            No += self.NoIMFs
-            self.Bs = Run(image[:, :, 2])
-            self.NoIMFs += No
+        if len(self.shape) == 3:
+            self.MeanFrequency = np.array([[], [], []])
         else:
-            Run(image)
+            self.MeanFrequency = np.array([])
+        self.stdFrequency = self.MeanFrequency.copy()
+
+        def Run(img: np.ndarray, n=0):
+
+            def emd_images_col(colOfImage: np.ndarray):
+                return self.EMD(colOfImage).decompose()
+
+            def concatZeros(row: np.ndarray, howMuch: int) -> np.ndarray:
+                if len(row.shape) == 1:
+                    return np.vstack((row, np.zeros((howMuch, row.shape[0]))))
+                return np.vstack((row, np.zeros((howMuch, row.shape[1]))))
+
+            def fftMyIMF(imf: np.ndarray) -> np.ndarray:
+                return fft.fft(imf).real
+
+            def decAndFFT(colOfImage: np.ndarray):
+                decCol = emd_images_col(colOfImage)
+                dft = fftMyIMF(decCol)
+
+                if len(self.MeanFrequency.shape) == 1:
+                    self.MeanFrequency = np.append(self.MeanFrequency, dft.mean(axis=1))
+                    self.stdFrequency = np.append(self.stdFrequency, dft.std(axis=1))
+
+                else:
+                    self.MeanFrequency[n] = np.append(self.MeanFrequency[n], dft.mean(axis=1))
+                    self.stdFrequency[n] = np.append(self.stdFrequency[n], dft.std(axis=1))
+
+
+
 
     def __call(self, imf, dtype=None) -> np.ndarray:
         if type(imf) == slice:
