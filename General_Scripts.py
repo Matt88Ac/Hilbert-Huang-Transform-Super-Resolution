9879d89b28bf51c078b44404fc0f1ff297c90efd
Matt8ac <Matt8ac@gmail.com>
2020-09-16 01:05:55 +0300

    features fixed

--- General_Scripts.py
+++ General_Scripts.py
@@ -11,22 +11,33 @@ Sharpen3x3 = Sharpen3x3.reshape((3, 3))
 def imread_Interactive(flags=None):
     path = os.getcwd()
     path = path.replace(path[2], '/')
-    path += '/DATA/'
-    lis = os.listdir(path)
+    temp = path.split('/')
+    while temp[len(temp) - 1] != 'Hilbert-Huang-Transform-Super-Resolution':
+        temp.pop()
+    newPath = ''
+
+    for t in temp:
+        newPath += t + '/'
+
+    os.chdir(newPath)
+
+    newPath += 'DATA/'
+    lis = os.listdir(newPath)
     name = ''
     spot = 0
+
     while True:
         print("\n \n \n")
         print(str(spot + 1) + ' of ' + str(len(lis)))
         if spot >= len(lis):
-            lis = os.listdir(path)
+            lis = os.listdir(newPath)
             spot = 0
             name = ''
             print("Error 1")
             continue
 
         if len(lis) == 0:
-            lis = os.listdir(path)
+            lis = os.listdir(newPath)
             name = ''
             spot = 0
             print("Error 2")
@@ -50,9 +61,24 @@ def imread_Interactive(flags=None):
         name += inp
         lis = list(filter(lambda x: name in x, lis))
         spot = 0
-    path = 'DATA/' + lis[spot]
-    return cv2.imread(path, flags)
+    newPath = 'DATA/' + lis[spot]
+    temp = cv2.imread(newPath, flags)
+    os.chdir(path)
+    return temp
 
 
 def imread(fname, flags):
-    return cv2.imread('DATA/' + fname, flags)
+    path = os.getcwd()
+    path = path.replace(path[2], '/')
+    temp = path.split('/')
+    while temp[len(temp) - 1] != 'Hilbert-Huang-Transform-Super-Resolution':
+        temp.pop()
+    newPath = ''
+
+    for t in temp:
+        newPath += t + '/'
+
+    os.chdir(newPath)
+    temp = cv2.imread('DATA/' + fname, flags)
+    os.chdir(path)
+    return temp
