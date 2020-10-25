b7c157e0d48665af11eeb2a6247402f0f892486a
Matt8ac <Matt8ac@gmail.com>
2020-10-25 22:02:53 +0200

    Update InterpolationsData_Impute.py

--- Old Data Filling Scripts/InterpolationsData_Impute.py
+++ Old Data Filling Scripts/InterpolationsData_Impute.py
@@ -7,6 +7,7 @@ def filterCols(df: pd.DataFrame, imf: str, interpol: str):
     inter_bool = (df['Interpolation Method'] == interpol).to_numpy()
     return df.loc[name_bool & inter_bool], name_bool, inter_bool
 
+
 def impute(df: pd.DataFrame, col: str):
     mean = df[col].mean()
     df[col] = df[col].fillna(value=mean)
@@ -14,16 +15,15 @@ def impute(df: pd.DataFrame, col: str):
 
 
 def Main():
-    df = pd.read_csv('interpolations.csv')
-    cols_to_impute = df.columns[8:].to_numpy(dtype=str)
+    df = pd.read_csv('new_data.csv')
+    cols_to_impute = df.columns[5:13].to_numpy(dtype=str)
 
     for i in range(1, 13):
         name = 'IMF ' + str(i)
         relevent_cols = df.loc[df.isnull().any(axis=1)]
         interpolations = relevent_cols['Interpolation Method'].unique()
-
+        relevent_cols = None
         for inter in interpolations:
-            relevent_cols = None
             filtered, n, b = filterCols(df, name, inter)
             for col in cols_to_impute:
                 df.loc[n & b] = impute(filtered, col)
@@ -31,5 +31,7 @@ def Main():
     df.to_csv('imputed_interpolations.csv', index=False, index_label=False)
 
 
-
-Main()
+#Main()
+daf = pd.read_csv('imputed_interpolations.csv')
+print(len(daf))
+#daf.to_csv('imputed_interpolations.csv', index=False, index_label=False)
